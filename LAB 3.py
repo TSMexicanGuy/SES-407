cc453ab169cf7c700d831205f0ae8f45b5dd82dd
TSMexicanGuy <lcastr20@asu.edu>
2026-02-05 11:49:37 -0700

    Update LAB 3.py

--- LAB 3.py
+++ LAB 3.py
@@ -13,13 +13,27 @@ def create_plot():
     plt.xlabel("counts")
     plt.ylabel("voltage")
 
-data = serial.Serial('COM6', 9600)
+
+
+
+
+data = serial.Serial('COM4', 9600)
 while True:
     while data.in_waiting==0:
         pass
     data1=data.readline()
-print(data1)
     
+    data2 = str(data1, 'utf-8')
+    niceData = int(data2.strip('\r\n'))
+    
+    volts = ((5-0)/(1023-0)) * niceData
+    print(volts)
+    voltage.append(volts)
+    drawnow(create_plot)
+    plt.pause(.0001)
+
+
+
 
 
 
